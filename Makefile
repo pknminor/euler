342e77619859d3bdceacbf7e0e3a3224ffb9beed
Pranav Natesh <pknminor@gmail.com>
2016-09-12 14:05:44 -0700

    up

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 def: run
 
+# to run script in interpreter use the command, for example execfile('clear.py')
+
+
 PROB="default"
 
 run:
 	python prob_${PROB}.py | tee std
+
+run_51:
+		/usr/bin/time python prob_51.py
 
 clean:
 		\rm -rf *log std
--- prob_51.py
+++ prob_51.py
@@ -1,15 +1,19 @@
- # Problem 51 By replacing the 1st digit of the 2-digit number *3, it turns out
- # that six of the nine possible values: 13, 23, 43, 53, 73, and 83, are all
- # prime.
- # 
- # By replacing the 3rd and 4th digits of 56**3 with the same digit, this 5-digit
- # number is the first example having seven primes among the ten generated
- # numbers, yielding the family: 56003, 56113, 56333, 56443, 56663, 56773, and
- # 56993. Consequently 56003, being the first member of this family, is the
- # smallest prime with this property.
- # 
- # Find the smallest prime which, by replacing part of the number (not necessarily
- # adjacent digits) with the same digit, is part of an eight prime value family.
+# Problem 51 By replacing the 1st digit of the 2-digit number *3, it turns out
+# that six of the nine possible values: 13, 23, 43, 53, 73, and 83, are all
+# prime.
+# 
+# By replacing the 3rd and 4th digits of 56**3 with the same digit, this 5-digit
+# number is the first example having seven primes among the ten generated
+# numbers, yielding the family: 56003, 56113, 56333, 56443, 56663, 56773, and
+# 56993. Consequently 56003, being the first member of this family, is the
+# smallest prime with this property.
+# 
+# Find the smallest prime which, by replacing part of the number (not necessarily
+# adjacent digits) with the same digit, is part of an eight prime value family.
+
+#  TODO:
+#  code cleanup, comment cleanup
+# 
 
 import sys
 import itertools
@@ -199,7 +203,7 @@ def prob_51():
     #for curr_num in range(10, 9999999):#
     #for curr_num in range(121300, 121413):#
     #for curr_num in range(120000, 121413):#
-    for curr_num in range(100, 121413):#
+    for curr_num in range(10000, 121413):#
 
         curr_num_tot_digits = num_digits(curr_num)
 
