03a2ec782cb83293b0c39cab4bd0fae1793a8578
Pranav Natesh <pknminor@gmail.com>
2016-09-13 16:13:06 -0700

    up

--- prob_54.py
+++ prob_54.py
@@ -24,15 +24,15 @@
 # and so on.
 
 # Consider the following five hands dealt to two players:
-# -------|-----------------------------------------------------------|--------------------------------------------------------|-------------------|---
-# Hand   |     Player 1                                              |     Player 2                                           |        Winner     |
-# -------|-----------------------------------------------------------|--------------------------------------------------------|-------------------|---
-# 1      |     5H 5C 6S 7S KD Pair of Fives                          |    2C 3S 8S 8D TD Pair of Eights                       |        Player 2   |
-# 2      |     5D 8C 9S JS AC Highest card Ace                       |    2C 5C 7D 8S QH Highest card Queen                   |        Player 1   |
-# 3      |     2D 9C AS AH AC Three Aces                             |    3D 6D 7D TD QD Flush  with Diamonds                 |        Player 2   |
-# 4      |     4D 6S 9H QH QC Pair of Queens    (Highest card Nine)  |    3D 6D 7H QD QS Pair of Queens (Highest card Seven)  |        Player 1   |
-# 5      |     2H 2D 4C 4D 4S Full House        (With Three Fours)   |    3C 3D 3S 9S 9D Full House (with Three Threes)       |        Player 1   |
-# -------|-----------------------------------------------------------|--------------------------------------------------------|-------------------|---
+# -------|------------------------------------------------------|----------------------------------------------------|----------|
+# Hand   | Player 1                                             |  Player 2                                          | Winner   |
+# -------|------------------------------------------------------|----------------------------------------------------|----------|
+# 1      | 5H 5C 6S 7S KD Pair of Fives                         | 2C 3S 8S 8D TD Pair of Eights                      | Player 2 |
+# 2      | 5D 8C 9S JS AC Highest card Ace                      | 2C 5C 7D 8S QH Highest card Queen                  | Player 1 |
+# 3      | 2D 9C AS AH AC Three Aces                            | 3D 6D 7D TD QD Flush  with Diamonds                | Player 2 |
+# 4      | 4D 6S 9H QH QC Pair of Queens    (Highest card Nine) | 3D 6D 7H QD QS Pair of Queens (Highest card Seven) | Player 1 |
+# 5      | 2H 2D 4C 4D 4S Full House        (With Three Fours)  | 3C 3D 3S 9S 9D Full House (with Three Threes)      | Player 1 |
+# -------|------------------------------------------------------|----------------------------------------------------|----------|
 
 # Player 1 The file, poker.txt, contains one-thousand random hands dealt
 # to two players. Each line of the file contains ten cards (separated by a
@@ -53,7 +53,101 @@
     # rank hand from [A-J]
     # compare highest cards
 # count wins for both players
-import sys, re
+import sys, re, subprocess, time
+
+class Card(object):
+
+    D = 1
+    C = 2
+    H = 3
+    S = 4
+
+    SUITS = {
+            'D':   'Diamonds',
+            'C':   'Clubs',
+            'H':   'Hearts',
+            'S':   'Spades'
+            }
+
+    # Define the names of special cards
+    VALUES = {
+             'A' : '1',
+             'T' : '10',
+             'J' : '11',
+             'Q' : '12',
+             'K' : '13'
+             }
+
+    SPECIAL_VALUES = {
+            '1': 'Ace',
+            '10': '10',
+            '11': 'Jack',
+            '12': 'Queen',
+            '13': 'King'
+             }
+
+    def __init__(self):
+        self.suit_full  = None
+        self.suit       = None
+        self.value      = None
+
+    def set_suit(self, suit):
+        self.suit      = str(suit)
+        self.suit_full = str(self.SUITS[suit])
+
+    def set_value(self, value):
+        if value.isdigit() == True:
+            self.value = str(value)
+        else:
+            self.value = str(self.VALUES[value])
+
+    def __set__(self, other):
+        self.suit_full = other.suit_full
+        self.suit      = other.suit
+        self.value     = other.value
+
+    def __str__(self):
+        # Return a string description of ourself
+        if self.value in self.SPECIAL_VALUES:
+            buf = self.SPECIAL_VALUES[str(self.value)]
+        else:
+            buf = str(self.value)
+
+        buf = buf + ' of ' + self.SUITS[str(self.suit)]
+        return buf
+
+    def __lt__(self, other):
+        # Compare the card with another card
+        # (return true if we are smaller, false if
+        # we are larger, 0 if we are the same)
+        if self.suit < other.suit:
+            return True
+        elif self.suit > other.suit:
+            return False
+
+        if self.value < other.value:
+            return True
+        elif self.value > other.value:
+            return False
+        return 0
+
+# hand is a set of 5 cards
+class Hand(object):
+    cardList = [Card() for i in range(5)]
+
+    def set_hand(self, otherCardList):
+        for i in range(5):
+            self.cardList[i].__set__(otherCardList[i])
+
+    # determine which hand is better, using the priority list in the problem
+    def __lt__(self, other):
+        pass
+
+    def __str__(self):
+        string = ''
+        for i in range(5):
+            string += self.cardList[i].__str__() + " ; "
+        return string
 
 player1_hands = []
 player2_hands = []
@@ -73,9 +167,20 @@ def main(argv):
     read_hands()
     player1_hands.reverse()
     player2_hands.reverse()
-    print player1_hands.pop()
-    print player2_hands.pop()
-    pass
+    #print player1_hands.pop()
+
+    # hand1
+    newplay1Hand1 = Hand()
+    play1hand1 = list(player1_hands.pop())
+
+    newCardList = [Card() for i in range(5)]
+
+    for i in range(5):
+        newCardList[i].set_value(play1hand1[i][0])
+        newCardList[i].set_suit(play1hand1[i][1])
+
+    newplay1Hand1.set_hand(newCardList)
+    print newplay1Hand1.__str__()
 
 if __name__ == "__main__":
     main(sys.argv)
